647e232c59179e10bc950de9b25280e447211f11
Георгий Князев <saint_georgy@yahoo.com>
2022-06-01 13:41:36 +0300

    Добавьте файлы проекта.

---
+++ 4 uslovniy operator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _4_uslovniy_operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomNamber = random.Next(0, 101);
+            Console.WriteLine(randomNamber);
+            int sumOfNumber = 0;
+
+            for (int i = 1; i <= randomNamber; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    sumOfNumber += i;
+                }
+                else if (i % 5 == 0)
+                {
+                    sumOfNumber += i;
+                }
+                else
+                    continue;
+            }
+            Console.WriteLine(sumOfNumber);
+        }
+    }
+}
