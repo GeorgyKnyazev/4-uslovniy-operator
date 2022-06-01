3ec57b9b7fdd87ab0849782180510c9ba786d170
Георгий Князев <saint_georgy@yahoo.com>
2022-06-01 16:37:48 +0300

    sa

--- 4 uslovniy operator/Program.cs
+++ 4 uslovniy operator/Program.cs
@@ -6,23 +6,21 @@ namespace _4_uslovniy_operator
     {
         static void Main(string[] args)
         {
+            int minimumRabdomNumber = 0;
+            int maximumRabdomNumber = 101;
             Random random = new Random();
-            int randomNamber = random.Next(0, 101);
+            int randomNamber = random.Next(minimumRabdomNumber, maximumRabdomNumber);
             Console.WriteLine(randomNamber);
             int sumOfNumber = 0;
+            int multipleNumber1 = 3;
+            int multipleNumber2 = 5;
 
             for (int i = 1; i <= randomNamber; i++)
             {
-                if (i % 3 == 0)
+                if (i % multipleNumber1 == 0 || i % multipleNumber2 == 0)
                 {
                     sumOfNumber += i;
                 }
-                else if (i % 5 == 0)
-                {
-                    sumOfNumber += i;
-                }
-                else
-                    continue;
             }
             Console.WriteLine(sumOfNumber);
         }
